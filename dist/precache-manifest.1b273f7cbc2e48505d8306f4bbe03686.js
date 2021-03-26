b8ab69c59b040a75238c7aa815b861f279035307
Horia <horia@hstudio.uk>
2021-03-26 21:28:39 +0000

    added built version;

---
+++ dist/precache-manifest.1b273f7cbc2e48505d8306f4bbe03686.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef068c3c6c70dae5738",
+    "url": "/css/about.f8390d93.css"
+  },
+  {
+    "revision": "7bdb47d23efd6606440d",
+    "url": "/css/app.44291274.css"
+  },
+  {
+    "revision": "c1c44a54126f793241da90e3d4c147e3",
+    "url": "/images/cat_1.png"
+  },
+  {
+    "revision": "df20cfaf244483116826f6df4ab4e8c8",
+    "url": "/images/cat_2.png"
+  },
+  {
+    "revision": "61c713eb943a9aca690a8d3d8b01f456",
+    "url": "/images/cat_3.png"
+  },
+  {
+    "revision": "ece95bcc9d1c9d5730beb374e7c29df8",
+    "url": "/images/cat_4.png"
+  },
+  {
+    "revision": "55b08e2f518b79136ff1c597e93306c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef068c3c6c70dae5738",
+    "url": "/js/about.0806eff9.js"
+  },
+  {
+    "revision": "7bdb47d23efd6606440d",
+    "url": "/js/app.4ba3a3d1.js"
+  },
+  {
+    "revision": "d0e4d80f747516fa48be",
+    "url": "/js/chunk-vendors.8efd9559.js"
+  },
+  {
+    "revision": "631724528636e9c02405d428fb250e54",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
